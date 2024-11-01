a27ac856cacfa4ae7d4ad51af65c947dc4e36ce5
elda2535 <gguevaralino@stetson.edu>
2024-11-01 10:30:46 -0400

    Initial commit

---
+++ src/EggMan/EggMan.java
@@ -0,0 +1,65 @@
+package EggMan;
+
+public class EggMan {
+
+	public static void main(String[] args) {
+		
+		// Creating first helloWorld
+		System.out.println("Hello World!");
+		 
+		// Blank space
+        System.out.println();
+	
+        // Creating a stop sign without "stop"
+		     System.out.println("----------");	     
+		   System.out.println("/           \\");
+		   System.out.println("/             \\");
+	      System.out.println("\\             /");
+	       System.out.println("\\           /");
+	        System.out.println("----------");
+	        
+	      // 2Blank spaces
+	        System.out.println();
+	        System.out.println();
+	            
+	     // Creating a half a stop sign without "stop"
+	       System.out.println("\\             /");
+		     System.out.println("\\           /");
+		     System.out.println("----------");
+		     System.out.println("+----------+");       
+		    
+		     // 2Blank spaces
+		    System.out.println();
+	        System.out.println();
+	            
+		         System.out.println("----------");	     
+			   System.out.println("/           \\");
+			   System.out.println("/    Stop    \\");
+		      System.out.println("\\             /");
+		       System.out.println("\\           /");
+		        System.out.println("----------");
+		    
+		        // 2Blank spaces
+		        System.out.println();
+		        System.out.println();
+		        
+		       System.out.println("----------");	     
+			System.out.println("/           \\");
+			System.out.println("/             \\");
+			   System.out.println("+----------+"); 
+			  
+			   //Blank space
+			   System.out.println();
+			   
+			   //output message as requested
+			   System.out.println("Go Hatters!!");
+			 
+			   //Blank space
+			   System.out.println();
+		
+			   //output my name
+			   System.out.println("Programed by: Grace Guevara Lino");
+
+	}
+
+}
